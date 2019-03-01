1dcac75c22fad82085eca62d9168d71ee7d73618
yanglijun <yanglj@halove.com>
2019-03-01 18:42:49 +0800

    增加绑定适配器的<指定自定义方法名称>相关示例；

---
+++ app/src/main/java/com/example/databindingdemo/AdapterActivity.java
@@ -0,0 +1,25 @@
+package com.example.databindingdemo;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.databindingdemo.databinding.ActivityAdapterBinding;
+
+/**
+ * @author yanglijun
+ * @date 19-3-1
+ */
+public class AdapterActivity extends AppCompatActivity {
+
+    private ActivityAdapterBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_adapter);
+
+        binding.setTintColor(getResources().getColorStateList(R.color.colorAccent));
+    }
+}
--- app/src/main/java/com/example/databindingdemo/util/BindAdapter.java
+++ app/src/main/java/com/example/databindingdemo/util/BindAdapter.java
@@ -1,12 +1,16 @@
 package com.example.databindingdemo.util;
 
 import android.databinding.BindingAdapter;
+import android.databinding.BindingMethod;
+import android.databinding.BindingMethods;
 import android.widget.ImageView;
 
 /**
  * @author yanglijun
  * @date 19-2-19
  */
+@BindingMethods({@BindingMethod(type = android.widget.ImageView.class, attribute = "android:tint",
+        method = "setImageTintList")})
 public class BindAdapter {
 
     @BindingAdapter("app:image")
