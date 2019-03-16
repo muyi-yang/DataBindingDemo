a8b7d8e6fc324b5a8e66c94ab297999bc992a8a0
yanglijun <yanglj@halove.com>
2019-03-16 13:11:32 +0800

    增加注释；

--- app/src/main/java/com/example/databindingdemo/util/Converter.java
+++ app/src/main/java/com/example/databindingdemo/util/Converter.java
@@ -11,13 +11,22 @@ import java.util.Date;
  * @date 19-3-13
  */
 public class Converter {
-
+    /**
+     * 绑定方法
+     * @param value
+     * @return
+     */
     @InverseMethod("stringToDate")
     public static String dateToString(long value) {
         SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return s.format(new Date(value));
     }
 
+    /**
+     * 逆转换器方法
+     * @param value
+     * @return
+     */
     public static long stringToDate(String value) {
         SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         long time = 0;
